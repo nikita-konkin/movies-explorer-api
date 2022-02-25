5cc230e7a373a61cb3f3a19a28cea064616246a7
Nikita Konkin <niki-konkin@yandex.ru>
2022-02-25 15:32:57 +0300

    bagfix

--- app.js
+++ app.js
@@ -66,8 +66,9 @@ app.post('/signin', celebrate({
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
+    name: Joi.string().min(3).max(30).required(),
     email: Joi.string().email().required(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().min(8).required(),
   }),
 }), createUser);
 
